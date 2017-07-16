d6f4b593d49651fea5d1c2c77a715623059c6242
Steve Konves <stephen.konves@gmail.com>
2017-07-16 15:08:07 -0700

    feat: add functional code

---
+++ index.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const request = require('superagent');
+
+module.exports = class Neo4jClient {
+	/**
+	 *
+	 * @param {string} serviceRoot (eg. https://127.0.0.1)
+	 * @param {string} authToken
+	 */
+	constructor(serviceRoot, authToken) {
+		this.serviceRoot = serviceRoot;
+		this.authToken = authToken;
+	}
+
+	/**
+	 * Runs one or more cypher queries
+	 * @param {Array<object>} statements
+	 * @returns {Promise<array>} the body
+	 */
+	query(statements) {
+		const body = {
+			statements: statements.map(s => {
+				return {
+					statement: s.statement,
+					parameters: s.parameters,
+					resultDataContents: [
+						//'row'
+					],
+					includeStats: false
+				};
+			})
+		};
+
+		return new Promise((resolve, reject) => {
+			request
+				.post(`${this.serviceRoot}/db/data/transaction/commit`)
+				.send(body)
+				.set('Authorization', `Bearer ${this.authToken}`)
+				.set('Content-Type', 'application/json')
+				.end((err, res) => {
+					if (err) {
+						reject(err);
+					} else if (res.body.errors && res.body.errors.length > 0) {
+						reject({ neo4j: { errors: res.body.errors, statements } });
+					} else {
+						const results = res.body.results;
+
+						if (results) {
+							resolve(this.mapResults(results));
+						} else {
+							reject(res.body);
+						}
+					}
+				});
+		});
+	}
+
+	mapResults(results) {
+		return results.map(result => {
+			const rows = result.data.map(rowData => {
+				let rowObject = {};
+
+				result.columns.forEach((columnName, i) => {
+					rowObject[columnName] = rowData.row[i];
+				});
+
+				return rowObject;
+			});
+
+			return rows;
+		});
+	}
+};
